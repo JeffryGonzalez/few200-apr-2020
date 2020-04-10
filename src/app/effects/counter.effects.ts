8a443d90b58d63a555c6036ebbb41265905d6e36
Jeffry Gonzalez <jeff@hypertheory.com>
2020-04-10 14:02:03 -0400

    before routing

--- src/app/effects/counter.effects.ts
+++ src/app/effects/counter.effects.ts
@@ -1,9 +1,22 @@
 import { Actions, ofType, createEffect } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { tap, map, filter } from 'rxjs/operators';
 import * as counterActions from '../actions/counter.actions';
+import { applicationStarted } from '../actions/app.actions';
 @Injectable()
 export class CounterEffects {
+
+  readCountBy$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(applicationStarted), // the right action? if not, stop here.
+      map(() => localStorage.getItem('by')), // "1", "3", "5" | null
+      filter(by => by !== null), // if it is null, stop here
+      filter(by => by === '1' || by === '3' || by === '5'), // being extra careful (is it a 1, 3 or 5?)
+      map(by => +by), // convert the string (e.g. '5', to a number, e.g. 5)
+      map(by => counterActions.countBySet({ by })) // dispatch an action back to the reducer.
+    )
+    , { dispatch: true } // dispatch maybe?
+  );
 
   saveCountBy$ = createEffect(() =>
     this.actions$.pipe(
